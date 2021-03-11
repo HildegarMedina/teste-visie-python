e030dad39419317fa88b6d7d5d1e38bcc1825725
Hildegar Medina <medinahildegar1@gmail.com>
2021-03-11 18:58:41 -0300

    Add person and delete

--- App.py
+++ App.py
@@ -1,5 +1,6 @@
 #Modules
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for
+from datetime import datetime
 from flask_mysqldb import MySQL
 
 #Initializations
@@ -15,18 +16,66 @@ app.secret_key = "mysecretkey"
 #Connect with database
 mysql = MySQL(app)
 
-#ROUTER
+#START: ROUTER
+
 #Home
 @app.route("/")
 def index():
 
     #Cursor
     cur = mysql.connection.cursor()
-    cur.execute("SELECT nome , data_admissao FROM pessoas") #id_pessoa = id
+    cur.execute("SELECT id_pessoa, nome , data_admissao FROM pessoas") #id_pessoa = id
     data = cur.fetchall()
+
+
     
     #Render
     return render_template("index.html", persons = data)
+
+#Add person
+@app.route("/add", methods = ["POST"])
+def addPerson(): 
+    if request.method == "POST":
+        name = request.form["name"]
+        date = request.form["date"]
+
+        #Verify datas
+        if name == "" or date == "":
+            #Send message
+            flash("Você precisa preencher todos os dados")
+        else: 
+            
+            #Format date
+            datatemp = date.split("-")
+            date = datatemp[0] + "/" + datatemp[2] + "/" + datatemp[1]
+            
+            #Cursor
+            cur = mysql.connection.cursor()
+            cur.execute("INSERT into pessoas (nome, data_admissao) VALUES(%s, %s)", (name, date))
+            mysql.connection.commit()
+
+            #Send message
+            flash("Pessoa adicionada com sucesso")
+
+        #Redirect
+        return redirect(url_for("index"))
+
+#Delete person
+@app.route("/delete/<id>")
+def deletePerson(id):
+
+    id = str(id)
+
+    #Cursor
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE from pessoas WHERE id_pessoa = {0}".format(id))
+    cur.connection.commit()
+
+    #Send message
+    flash("Pessoa eliminada com sucesso ")
+    return redirect(url_for("index"))
+
+#END: ROUTER
 
 #Run server
 if __name__ == '__main__':
